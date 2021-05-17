6696ed8c5a717e2f1126c56fc97e51e340574ba3
Dmytro <Dmytro@DESKTOP-5CG8V75>
2021-05-17 12:18:18 +0300

    Add .py file

---
+++ test_lab_5.py
@@ -0,0 +1,15 @@
+num_list = []
+while True:
+    n = input("Input int number")
+    if n == "end":
+        break
+    num_list.append(int(n))
+
+theSum = 0
+for i in range(len(num_list)):
+    num_list[i] = num_list[i] * num_list[i]
+
+for i in num_list:
+    theSum = theSum + i
+
+print(theSum)
